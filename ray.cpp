c1e25cd3ff4a014f123faf5735ca60829591c6f4
Tiago Ferreira <Tiago.Ferreira@criticaltechworks.com>
2021-02-07 20:26:08 +0000

    Minor change

--- ray.cpp
+++ ray.cpp
@@ -4,5 +4,5 @@ Ray::Ray(const Vec3f& _origin, const Vec3f& _direction)
   : origin_{_origin}, direction_{_direction} {}
 
 std::ostream& operator<<(std::ostream& os, const Ray& ray) {
-    return os << "[Origin: " << ray.origin_ << " Direction: " << ray.direction_ << "]";
+    return os << "Origin: " << ray.origin_ << " Direction: " << ray.direction_;
 }
